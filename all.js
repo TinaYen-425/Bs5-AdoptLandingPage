c567c2bf76cf8dcc5ac8c82b203b8e970354b6d2
TinaYen-425 <b944326@gmail.com>
2022-03-07 20:49:07 +0800

    first commit

---
+++ all.js
@@ -0,0 +1,23 @@
+    // const link = document.querySelector('.link-custom-secondary');
+    // const OpenPic = document.querySelector('.OpenPic');
+    // link.addEventListener('mouseenter', (e) =>{
+    //     let change = (OpenPic.className == 'card-img rounded-top-4 OpenPic opacity-0')
+    //     if (change == true){
+    //         OpenPic.classList.remove('opacity-0');
+    //         OpenPic.classList.add('opacity-100');
+    //     }else{
+    //         OpenPic.classList.remove('opacity-100');
+    //         OpenPic.classList.add('opacity-0');
+    //     }
+    // });
+    // link.addEventListener('mouseleave', (e) =>{
+    //     let change = (OpenPic.className == 'card-img rounded-top-4 OpenPic opacity-0')
+    //     if (change == true){
+    //         OpenPic.classList.remove('opacity-0');
+    //         OpenPic.classList.add('opacity-100');
+    //     }else{
+    //         OpenPic.classList.remove('opacity-100');
+    //         OpenPic.classList.add('opacity-0');
+    //     }
+    // });
+
